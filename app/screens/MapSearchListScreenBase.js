c3984fee7b9cf5896471192dff9c327e462f3024
Vietbrother <anhmv.uet@gmail.com>
2019-02-26 18:27:29 +0700

    bo sung collasple and expand

--- app/screens/MapSearchListScreenBase.js
+++ app/screens/MapSearchListScreenBase.js
@@ -1,72 +1,133 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
-  ListView
-} from 'react-native';
+  ListView,
+  Text,
+  ScrollView
+} from 'react-native'
 
-
-import ThemeService from '../util/ThemeService';
-import api from '../util/ApiMock';
+import ThemeService from '../util/ThemeService'
+import api from '../util/ApiMock'
 import { RkConfig } from 'react-native-ui-kitten'
+
+import Panel from '../components/common/Panel'
 
 export default class MapSearchListScreenBase extends Component {
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     let ds = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2,
-    });
+    })
     let data = api.getAddressInfo();
     this.state = {
       dataSource: ds.cloneWithRows(data)
-    };
+    }
+
+    let districts = api.getAddressInfo();
+
+
+
   }
 
-  render() {
-     let Header = ThemeService.getMapListHeaderComponent();
+  render () {
+    let Header = ThemeService.getMapListHeaderComponent()
+
     return (
       <View style={styles.container}>
         <Header/>
-        <ListView
-          style={styles.list}
-          automaticallyAdjustContentInsets={false}
-          dataSource={this.state.dataSource}
-          renderRow={(row) => this._renderRowItem(row)}
-        />
+        {/*<View style={styles.content}>*/}
+          {/*<Panel title="This is the first container" style={styles.panel}>*/}
+            {/*<Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore*/}
+              {/*et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip*/}
+              {/*ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu*/}
+              {/*fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt*/}
+              {/*mollit anim id est laborum.</Text>*/}
+          {/*</Panel>*/}
+          {/*<Panel title="Second panel in the view" style={styles.panel}>*/}
+            {/*<Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Duis aute irure dolor in reprehenderit in*/}
+              {/*voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non*/}
+              {/*proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>*/}
+          {/*</Panel>*/}
+          {/*<Panel expanded title="One more to test" style={styles.panel}>*/}
+            {/*<Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore*/}
+              {/*et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip*/}
+              {/*ex ea commodo consequat.</Text>*/}
+            {/*<Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore*/}
+              {/*et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip*/}
+              {/*ex ea commodo consequat.</Text>*/}
+          {/*</Panel>*/}
+
+        {/*</View>*/}
+        <ScrollView>
+          {this._renderDistrict()}
+        </ScrollView>
+        {/*<ListView*/}
+          {/*style={styles.list}*/}
+          {/*automaticallyAdjustContentInsets={false}*/}
+          {/*dataSource={this.state.dataSource}*/}
+          {/*renderRow={(row) => this._renderRowItem(row)}*/}
+        {/*/>*/}
       </View>
     )
   }
 
-  _renderRowItem(rowData) {
-    let MapItemDetail = ThemeService.getMapItemComponent();
+  _renderDistrict () {
+    let data = api.getAddressInfo();
+    let districts = []
+    for (district of data) {
+      districts.push(
+        <Panel title={district.name} style={styles.panel}>
+          <ListView
+            style={styles.list}
+            automaticallyAdjustContentInsets={false}
+            dataSource={this.state.dataSource}
+            renderRow={(row) => this._renderRowItem(row)}
+          />
+        </Panel>
+      );
+    }
+    return districts;
+  }
+
+  _renderRowItem (rowData) {
+    let MapItemDetail = ThemeService.getMapItemComponent()
     return (
       <MapItemDetail
         style={styles.list}
         addressDetail={rowData}
         onClick={(rowData) => this._openAddressDetail(rowData)}
       />
-    );
+    )
   }
 
-  _openAddressDetail(rowData) {
+  _openAddressDetail (rowData) {
     this.props.navigator.push({
       screen: ThemeService.getMapSearchScreen(true),
       passProps: {
         addressId: rowData.id,
         addressName: rowData.name
       }
-    });
+    })
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
   },
   list: {
     backgroundColor: 'white',
     paddingLeft: 15
   },
-});
+  content: {
+    padding: 10,
+    backgroundColor: '#ecf0f1',
+    flex: 1,
+  },
+  panel: {
+    marginBottom: 10,
+  }
+})
--- app/screens/MapSearchScreenBase.js
+++ app/screens/MapSearchScreenBase.js
@@ -7,7 +7,9 @@ import {
   StatusBar,
   ListView,
   ScrollView,
-  Dimensions
+  Dimensions,
+  TouchableOpacity,
+  Platform, TextInput
 } from 'react-native'
 
 import _ from 'lodash'
@@ -15,20 +17,29 @@ import { RkConfig, RkButton, RkText, RkTextInput, RkBoardUpView, RkBarBg } from 
 import api from '../util/ApiMock'
 import ThemeService from '../util/ThemeService'
 
-import MapView, { MAP_TYPES, Polygon, ProviderPropType } from 'react-native-maps'
+// import MapView, { MAP_TYPES, Polygon, ProviderPropType } from 'react-native-maps'
+import MapView, { ProviderPropType, Marker, AnimatedRegion, Polygon, Callout } from 'react-native-maps'
 
-const { width, height } = Dimensions.get('window')
+const screen = Dimensions.get('window')
 
-const ASPECT_RATIO = width / height
-const LATITUDE = 37.78825
-const LONGITUDE = -122.4324
-const LATITUDE_DELTA = 0.0922
+const ASPECT_RATIO = screen.width / screen.height
+const LATITUDE = 21.012100
+const LONGITUDE = 105.812612
+const LATITUDE_DELTA = 0.0002
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
+// const { width, height } = Dimensions.get('window')
+//
+// const ASPECT_RATIO = width / height
+// const LATITUDE = 37.78825
+// const LONGITUDE = -122.4324
+// const LATITUDE_DELTA = 0.0922
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
 export default class MapSearchScreenBase extends Component {
 
   constructor (props) {
-    super(props);
+    super(props)
 
     this.state = {
       region: {
@@ -37,6 +48,26 @@ export default class MapSearchScreenBase extends Component {
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
       },
+      coordinate: new AnimatedRegion({
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+      }),
+    }
+  }
+
+  animate () {
+    const { coordinate } = this.state
+    const newCoordinate = {
+      latitude: LATITUDE + ((Math.random() - 0.5) * (LATITUDE_DELTA / 2)),
+      longitude: LONGITUDE + ((Math.random() - 0.5) * (LONGITUDE_DELTA / 2)),
+    }
+
+    if (Platform.OS === 'android') {
+      if (this.marker) {
+        this.marker._component.animateMarkerToCoordinate(newCoordinate, 50)
+      }
+    } else {
+      coordinate.timing(newCoordinate).start()
     }
   }
 
@@ -55,24 +86,139 @@ export default class MapSearchScreenBase extends Component {
     )
   }
 
-  _renderMapContent() {
+  _renderMapContent () {
     return (
-      <RkBoardUpView>
-        <MapView
-          style={styles.map}
-          initialRegion={this.state.region}
-        >
 
+      <View style={styles.container}>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+        >
+          <Polygon
+            coordinates={[
+              { latitude: 21.012100, longitude: 105.812612 },
+              { latitude: 21.012126, longitude: 105.812672 },
+              { latitude: 21.012093, longitude: 105.812868 },
+              { latitude: 21.011824, longitude: 105.812728 }
+
+            ]}
+            strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+            strokeColors={[
+              '#7F0000',
+              '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+              '#B24112',
+              '#E5845C',
+              '#238C23',
+              '#7F0000'
+            ]}
+            fillColor="#fff68f"
+            strokeWidth={1}
+          />
+
+          <Polygon
+            coordinates={[
+              { latitude: 21.012126, longitude: 105.812612 },
+              { latitude: 21.012156, longitude: 105.812672 },
+              { latitude: 21.012093, longitude: 105.812868 },
+              { latitude: 21.011924, longitude: 105.812728 }
+
+            ]}
+            strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+            strokeColors={[
+              '#7F0000',
+              '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+              '#B24112',
+              '#E5845C',
+              '#238C23',
+              '#7F0000'
+            ]}
+            fillColor="#7F0000"
+            strokeWidth={1}
+          />
+          <Marker.Animated
+            ref={marker => { this.marker = marker }}
+            coordinate={this.state.coordinate}
+          />
+
+
+          <Callout>
+            <View style={styles.calloutView}>
+              <TextInput style={styles.calloutSearch}
+                         placeholder={'Search'}
+              />
+            </View>
+          </Callout>
         </MapView>
-      </RkBoardUpView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.animate()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Animate</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     )
   }
 
 }
 
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+  // on the style sheet
+  calloutView: {
+    flexDirection: 'row',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderColor: 'rgba(0,128,128, 0.9)',
+    borderRadius: 10, borderWidth: 1,
+    width: '80%',
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginTop: 40,
+    justifyContent: 'center',
+  },
+  calloutSearch: {
+    borderColor: 'rgba(0,128,128, 0.9)',
+    marginLeft: 20,
+    width: '90%',
+    marginRight: 10,
+    height: 40,
+    borderWidth: 0.0
+  }
 
 })
